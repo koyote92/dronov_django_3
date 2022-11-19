a6b5897a8156a3e9deceff5c1e0bebfa49870012
Roman Inozemtsev <irs1992@yandex.ru>
2022-11-20 02:53:57 +0500

    Second chapter is done with minor changes

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'bboard'
 
 urlpatterns = [
-    path('', views.index),
+    path('add/', views.BbCreateView.as_view(), name='add'),
+    path('<int:rubric_id>/', views.by_rubric, name='by_rubric'),
+    path('', views.index, name='index'),
 ]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-from .models import Bb
+from .models import Bb, Rubric
+from .forms import BbForm
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('bboard:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
 
 def index(request):
-    template = 'bboard/index.html'
     bbs = Bb.objects.all()
-    context = {'bbs': bbs}
-    return render(request, template, context)
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics,
+               'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
