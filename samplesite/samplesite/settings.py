2d1033be61facb10551604ab5c59e343c2dc2d7f
Roman Inozemtsev <irs1992@yandex.ru>
2022-11-20 01:16:20 +0500

    Oops.

--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$$ot4kpoi8_@574lko0yiy7#gs6v4c73rfoo7p6h*^qs58+351'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
